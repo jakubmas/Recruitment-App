247eab03a44496d4df64aace25e2e4ae601287bd
jakubmas <jak.maslowski@gmail.com>
2024-10-03 20:07:02 +0200

    feat: Fix TypeScript build errors by upgrading TypeScript to latest version, Updated Dropdown Component to resolve TypeScript issue, docs: Updated fe docs to stay consistent

--- fe/README.md
+++ fe/README.md
@@ -1,25 +1,25 @@
-### task
+### Front End documentation
 
 Install dependencies.
 
 ```bash
-yarn install
+npm install
 ```
 
 Serve with hot reload at <http://localhost:5173>.
 
 ```bash
-yarn run dev
+npm run dev
 ```
 
 ### Lint
 
 ```bash
-yarn run lint
+npm run lint
 ```
 
 ### Build
 
 ```bash
-yarn run build
+npm run build
 ```
--- fe/src/components/dropdown/index.tsx
+++ fe/src/components/dropdown/index.tsx
@@ -4,7 +4,7 @@ import { FiltersContextType, useFilterContext } from '../../contexts/filters';
 import { ChevronDown } from 'react-feather';
 
 export interface DropdownOption {
-  name: FiltersContextType['filters'][keyof FiltersContextType['filters']];
+  name: string | number;
   title?: string;
 }
 
@@ -15,7 +15,9 @@ interface DropdownProps {
 
 export const Dropdown = ({ options, filter }: DropdownProps) => {
   const { setFilters, filters } = useFilterContext();
-  const [selected, setSelected] = useState<DropdownOption>({ name: filters[filter] });
+  const [selected, setSelected] = useState<DropdownOption>({
+    name: filters[filter] as string | number,
+  });
 
   const handleChange = (option: DropdownOption) => {
     setSelected(option);
@@ -28,7 +30,7 @@ export const Dropdown = ({ options, filter }: DropdownProps) => {
     <div>
       <Listbox value={selected} onChange={handleChange}>
         <div className="relative mt-1">
-          <Listbox.Button className="relative w-full cursor-default bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+          <Listbox.Button className="relative w-full cursor-default bg-white py-2 pl-3 pr-10 text-left focus:outline-none sm:text-sm">
             <span className="block truncate">{selectedTitle}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronDown />
@@ -40,7 +42,7 @@ export const Dropdown = ({ options, filter }: DropdownProps) => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto bg-white text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto bg-white text-base shadow-lg focus:outline-none sm:text-sm">
               <Listbox.Option
                 key="all"
                 value={{ name: '' }}
@@ -52,7 +54,7 @@ export const Dropdown = ({ options, filter }: DropdownProps) => {
               </Listbox.Option>
               {options.map((option) => (
                 <Listbox.Option
-                  key={option.name}
+                  key={option.name.toString()}
                   value={option}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 px-4 ${active ? 'bg-gray-100' : ''}`
